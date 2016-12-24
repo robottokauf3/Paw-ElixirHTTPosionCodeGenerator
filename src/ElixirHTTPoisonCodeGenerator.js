69ed67b01f975ff7a3e6340b857b18d535aeb9d9
Rob Kaufmann <rok3@rok3.me>
2016-12-24 18:46:16 -0500

    Add support for query parameters

--- src/ElixirHTTPoisonCodeGenerator.js
+++ src/ElixirHTTPoisonCodeGenerator.js
@@ -2,11 +2,30 @@
 /* global registerCodeGenerator */
 
 import Mustache from 'mustache';
+import YouAreI from 'youarei';
 
 class ElixirHTTPoisonCodeGenerator {
   static identifier = 'me.rok3.PawExtensions.ElixirHTTPoisonCodeGenerator';
   static title = 'Elixir (HTTPoison)';
   static fileExtension = 'ex';
+
+  url(request) {
+    let rawUrl = new YouAreI(request.url);
+    let rawParams = new YouAreI(request.url).query_get();
+
+    let params = [];
+
+    for (let name of Object.keys(rawParams)) {
+      params.push({name: name, value: rawParams[name]});
+    }
+
+    return {
+      base: rawUrl.query_clear(),
+      fullPath: request.url,
+      hasParams: params.length > 0,
+      params: params
+    };
+  }
 
   headers(request) {
     const headerNames = Object.keys(request.headers);
@@ -29,6 +48,7 @@ class ElixirHTTPoisonCodeGenerator {
 
     const view = {
       request: request,
+      url: this.url(request),
       headers: this.headers(request)
     };
 
